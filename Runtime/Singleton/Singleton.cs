3c9e7ea28c16dc19e12823b1e48f0abdd80294cc
Mrinin <yagizsselimoglu@gmail.com>
2026-08-17 15:27:36 +0300

    Singleton: added on destroy on load

--- Runtime/Singleton/Singleton.cs
+++ Runtime/Singleton/Singleton.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
 
 namespace YShared.Singleton
-{    
-    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{   
+    [System.Obsolete("Use YSingleton instead.", false)]
+    public class Singleton<T> : YSingleton<T> where T : MonoBehaviour
+    {
+        
+    } 
+
+    /// <summary>
+    /// Singeton.
+    /// </summary>
+    /// <typeparam name="T">This class</typeparam>
+    public abstract class YSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         public static T Instance { get; private set; }
         public virtual bool CallDontDestroyOnLoad { get => true; }
@@ -20,5 +30,26 @@ namespace YShared.Singleton
             if (CallDontDestroyOnLoad)
                 DontDestroyOnLoad(gameObject);
         }
+
+        protected virtual void OnDestroy()
+        {
+            if (Instance == this)
+                Instance = null;
+        }
+    }
+
+    public abstract class YSceneSingleton<T> : YSingleton<T> where T : MonoBehaviour
+    {
+        public override bool CallDontDestroyOnLoad => false;
+
+        public static new T Instance {
+            get
+            {
+                return YSingleton<T>.Instance != null
+                ? YSingleton<T>.Instance
+                : null;
+            }
+        }
+
     }
 }
